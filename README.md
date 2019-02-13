99b16c662cc2987d813a8b2d974a0476d44f2cfd
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2019-02-13 17:50:22 +0100

    Updated publications

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # shoya.io
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/e0486fd4-3525-46ef-9f84-e111ffe3bab5/deploy-status)](https://app.netlify.com/sites/hungry-archimedes-92ccca/deploys)
+
+## dev
+
+```
+$ hugo server -D --gc --minify
+```
+
+## update publications
+
+1. Update `./reference.bib`
+
+2. Put preprints to `./static/preprint/`
+
+3. Run the script
+```
+$ python pub.py
+>> ./layouts/partials/publications/*
+>> and ./static/bibtex/* will be generated
+```
--- pub.py
+++ pub.py
@@ -1,12 +1,16 @@
-import bibtexparser
+import os
 import json
+import bibtexparser
+from bibtexparser.bwriter import BibTexWriter
+from bibtexparser.bibdatabase import BibDatabase
 
 with open('./reference.bib') as f:
     bdata = bibtexparser.load(f)
 
 for entry in bdata.entries:
     output = ''
-    authors_data = entry['author'].split('and')
+    key =  entry['ID']
+    authors_data = entry['author'].split(' and ')
     for i, author in enumerate(authors_data):
         name = ' '.join(reversed([n.strip() for n in author.split(',')]))
         if name == 'Shoya Ishimaru':
@@ -14,10 +18,10 @@ for entry in bdata.entries:
         if i == 0:
             output = name
         elif i == len(authors_data) - 1:
-            output += ' and ' + name + '. '
+            output += ' and ' + name
         else:
             output += ', ' + name
-    output += '<b>&#147;' + entry['title'] + '&#148;</b>. '
+    output += '. <b>&#147;' + entry['title'] + '&#148;</b>. '
 
     if 'journal' in entry.keys():
         output += ' ' + entry['journal'] + ', '
@@ -30,12 +34,21 @@ for entry in bdata.entries:
         else:
             prefix = 'p.&nbsp;'
         output += prefix + entry['pages'] + ', '
-    output += entry['year']
+    output += entry['year'] + '.'
+
+    if os.path.exists('./static/preprint/' + key + '.pdf'):
+        output += ' <a href="/preprint/' + key + '.pdf" target="_blank">PDF</a>'
+    output += ' <a href="/bibtex/' + key + '.bib" target="_blank">BibTeX</a>'
+
     output = output.replace('{\\ss}', '&szlig;')
     output = output.replace('{\\"O}', '&Ouml;')
-    output = output.replace('Dayan Herurkar, a', 'Dayananda Herurkar')
     output = output.replace('--', '&ndash;')
-    output = '<p>' + output + '.</p>'
+    output = '<p>' + output + '</p>'
 
-    with open('./layouts/partials/publications/' + entry['ID'] + '.html', "w") as f:
+    with open('./layouts/partials/publications/' + key + '.html', "w") as f:
         f.write(output)
+
+    bout = BibDatabase()
+    bout.entries = [entry]
+    with open('./static/bibtex/' + key + '.bib', 'w') as f:
+        f.write(BibTexWriter().write(bout))
